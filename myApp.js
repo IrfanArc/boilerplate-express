0b46314d7a72ba61a632b6342da7278c3e5c31ca
Irfan Rashid <irfan.rashid@telenordigital.com>
2022-10-22 01:12:55 +0500

    using env variables

--- myApp.js
+++ myApp.js
@@ -1,5 +1,7 @@
+require('dotenv').config();
 let express = require('express');
 let app = express();
+
 console.log("Hello World");
 app.use('/public', express.static(__dirname + '/public'));
 // app.get('/',function(req, res){
@@ -9,7 +11,11 @@ app.get('/',function(req,res){
     res.sendFile(__dirname + '/views/index.html');
 });
 app.get('/json', function(req,res) {
-    res.json({'message':'Hello json'});
+    if(process.env.MESSAGE_STYLE === 'uppercase') {
+        res.json({'message':'HELLO JSON'});
+    } else {
+        res.json({'message':'Hello json'});
+    }
 });
 
 
