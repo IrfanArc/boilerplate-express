046a38993a6d049a95bdd8d48e4ed5c827c64ff7
Irfan Rashid <irfan.rashid@telenordigital.com>
2022-10-22 00:51:43 +0500

    serving json on /json route

--- myApp.js
+++ myApp.js
@@ -8,6 +8,9 @@ app.use('/public', express.static(__dirname + '/public'));
 app.get('/',function(req,res){
     res.sendFile(__dirname + '/views/index.html');
 });
+app.get('/json', function(req,res) {
+    res.json({'message':'Hello JSON'});
+});
 
 
 
