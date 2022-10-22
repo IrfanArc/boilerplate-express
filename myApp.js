b3a5dfefaf51ca218afa12232b0cb088aa7f6d09
Irfan Rashid <irfan.rashid@telenordigital.com>
2022-10-22 11:52:21 +0500

    Adding a POST handler on /name toute

--- myApp.js
+++ myApp.js
@@ -42,7 +42,9 @@ app.get('/:word/echo', function(req, res) {
     res.json({'echo':req.params.word});
 });
 
-app.route('/name').get(function(req, res){res.json({'name':req.query.first + " " + req.query.last})});
+app.route('/name').get(function(req, res){res.json({'name':req.query.first + " " + req.query.last})}).post(function(req, res){
+    res.json({'name':req.body.first + " " + req.body.last})    
+});
 
 
 
